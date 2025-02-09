c288279a74157484c5a3cbc100316033f8f19df0
Nguyen Xuan Quang <quang456a@gmail.com>
2025-02-09 22:44:09 +0700

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full z-50 bg-background/50 backdrop-blur-2xl">
+      <div className="max-w-7xl mx-auto px-6 py-3">
+<div className="flex items-center justify-between">
+
+</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
