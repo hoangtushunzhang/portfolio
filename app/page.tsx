1ad0a373cb641b243b2b228d9b5238517a85274d
Nguyen Xuan Quang <quang456a@gmail.com>
2025-02-16 10:50:08 +0700

    Hero section first state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
-  return <div className="min-h-creen bg-gray-900">
-    <Navbar />
-  </div>;
+  return (
+    <div className="min-h-creen bg-gray-900">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
