fc8d5b37df2f6fbe76cc3fdfe512c6fb19f95e20
Nguyen Xuan Quang <quang456a@gmail.com>
2025-02-17 12:29:35 +0700

    add gravity

---
+++ app/components/Preview.tsx
@@ -0,0 +1,86 @@
+import Gravity, { MatterBody } from "./Gravity";
+
+export default function Preview() {
+  return (
+    <div
+      className="py-20 sm:py-28 md:py-36 mb-6 sm:mb- max-w-7xl mx-auto relative 
+    bg-gradient-to-r from-primary/10 via-secondary/10 to-tertiary/10 rounded-2xl overflow-hidden"
+    >
+      <Gravity gravity={{ x: 0, y: 1 }} className="w-full h-full">
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="30%"
+          y="10%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#0015ff] text-white rounded-full hover:cursor-pointer px-8 py-4">
+            React
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="30%"
+          y="30%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#e794da] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+            Typescript
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="40%"
+          y="20%"
+          angle={10}
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#1f464d] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+            Motion
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="75%"
+          y="10%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#ff5941] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+            Tailwind
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="80%"
+          y="20%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#f97316] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+            Shadcn
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="50%"
+          y="10%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#ffd726] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+           Next.js
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="50%"
+          y="10%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#CD009F] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+           HTML
+          </div>
+        </MatterBody>
+        <MatterBody
+          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
+          x="50%"
+          y="10%"
+        >
+          <div className="text-xl sm:text-2xl md:text-3xl bg-[#00CD9F] text-white rounded-full hover:cursor-grab px-8 py-4 ">
+          CSS
+          </div>
+        </MatterBody>
+      </Gravity>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import Preview from "./components/Preview";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Main />
+      <Preview/>
       <Footer />
     </div>
   );
